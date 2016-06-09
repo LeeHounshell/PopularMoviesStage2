cff1a88ad3261e4ef700082ee583e6b81feef0e1
Lee Hounshell <lee.hounshell@gmail.com>
2016-06-08 20:01:07 -0700

    feat: load SQL test data inline

--- app/src/main/java/com/smartvariables/lee/popularmovies2/util/Tmdb_API_KEY.java
+++ app/src/main/java/com/smartvariables/lee/popularmovies2/util/Tmdb_API_KEY.java
@@ -4,7 +4,7 @@ public class Tmdb_API_KEY {
     private final static String TAG = "LEE: <" + Tmdb_API_KEY.class.getSimpleName() + ">";
     final private static String TMDB_BASE_URL = "http://image.tmdb.org/t/p/";
 
-    final private static String API_KEY = "<YOUR TMDB API KEY>"; // <--------- CHANGE THE API KEY!
+    final private static String API_KEY = "ee543441648038963595e9e2c0058c0f";
 
     public static String getBaseUrl() {
         return TMDB_BASE_URL;
